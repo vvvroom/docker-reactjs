ffe266db9221785fbdac44dff727267d632f0cd9
Budi Arsana <gusdecool@gmail.com>
2020-04-10 13:50:15 +0800

    Add react router

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,11 +1,34 @@
 import * as React from 'react';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from 'react-router-dom';
 
-export default class App extends React.Component{
-    public render(): React.ReactElement<HTMLDivElement> {
+export default class App extends React.Component {
+    public render(): React.ReactElement<Router> {
         return (
-            <div>
-                <h1>It's working</h1>
-            </div>
+            <Router>
+                <nav>
+                    <ul>
+                        <li>
+                            <Link to="/">Home</Link>
+                        </li>
+                        <li>
+                            <Link to="/about">About</Link>
+                        </li>
+                    </ul>
+                </nav>
+                <Switch>
+                    <Route path="/about">
+                        <h1>This is about page, HTTP rewrite is working.</h1>
+                    </Route>
+                    <Route path="/">
+                        <h1>This is home page, it's working.</h1>
+                    </Route>
+                </Switch>
+            </Router>
         );
     }
 }
